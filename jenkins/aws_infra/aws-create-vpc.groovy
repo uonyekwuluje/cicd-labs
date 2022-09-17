18405d968f47b1ba2b2b18fd57b9f4dee3a64947
uonyekwuluje <uionyekwuluje@gmail.com>
2022-09-17 06:01:02 -0400

    update test

--- jenkins/aws_infra/aws-create-vpc.groovy
+++ jenkins/aws_infra/aws-create-vpc.groovy
@@ -9,19 +9,28 @@ pipeline {
       label 'base'
   }
   
-  parameters {
-	choice choices: ['production', 'uat', 'dev'], description: '', name: 'Environment'
-	string(name: 'StackName',
-	       trim: true,
-	       description: 'Enter Stack.')
-    validatingString(name: 'StackName',
-			 defaultValue: '',
-			 failedValidationMessage: 'Stack name can only consist of Alphanumeric and dash characters',
-			 description: 'The name of the CloudFormation stack to create',
-			 regex: '[-a-zA-Z0-9]+')
-  }
   
   stages{
+    stage("Set Parameters") {
+      steps {
+        script {
+          properties([
+               parameters {
+                  choice choices: ['production', 'uat', 'dev'], description: '', name: 'Environment'
+                  string(name: 'StackName',
+                     trim: true,
+                     description: 'Enter Stack.')
+                  validatingString(name: 'StackName',
+                     defaultValue: '',
+                     failedValidationMessage: 'Stack name can only consist of Alphanumeric and dash characters',
+                     description: 'The name of the CloudFormation stack to create',
+                     regex: '[-a-zA-Z0-9]+')
+               }
+          ])
+        }
+      }
+    }
+
     stage('Test Parameters') {
 	    steps {
 		  script {
