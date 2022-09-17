19b55df12aa6af730cd5f484343aa41dd4fc4b18
uonyekwuluje <uionyekwuluje@gmail.com>
2022-09-17 05:45:48 -0400

    update pipeline

---
+++ jenkins/aws_infra/aws-create-vpc.groovy
@@ -0,0 +1,34 @@
+def instance_types = [
+    "bastion",
+    "ossec_server",
+    "ossec_agent"
+]
+
+pipeline {
+  agent {
+      label 'base'
+  }
+  
+  parameters {
+	choice choices: ['production', 'uat', 'dev'], description: '', name: 'Environment'
+	string(name: 'StackName',
+	       trim: true,
+	       description: 'Enter Stack.')
+    validatingString(name: 'VStackName',
+			 defaultValue: '',
+			 failedValidationMessage: 'Stack name can only consist of Alphanumeric and dash characters',
+			 description: 'The name of the CloudFormation stack to create',
+			 regex: '[-a-zA-Z0-9]+')
+  }
+  
+  stages{
+    stage('Test Parameters') {
+	    steps {
+		  script {
+			 def message = "AWS StackName => ${params.StackName} in Environment => ${params.Environment}"
+			 echo message
+		}
+	  }
+	}
+  }
+}
