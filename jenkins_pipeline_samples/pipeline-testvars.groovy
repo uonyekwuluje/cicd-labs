fac33edd6e3f5dcda14ddbdf2fcdaf5fdc9c7fd8
uonyekwuluje <uionyekwuluje@gmail.com>
2022-09-17 05:27:03 -0400

    update vars

--- jenkins_pipeline_samples/pipeline-testvars.groovy
+++ jenkins_pipeline_samples/pipeline-testvars.groovy
@@ -2,36 +2,11 @@ pipeline {
   agent {
       label 'base'
   }
-
-  parameters {
-    	choice name: 'NewEnvironment', choices: ['stg1', 'operations', 'production', 'stg2', 'stg3'], description: ''
-
-	    choice name: 'NewInstanceType', choices: instance_types, description: ''
-
-	    validatingString(name: 'StackName',
-			 defaultValue: '',
-			 failedValidationMessage: 'Stack name can only consist of Alphanumeric and dash characters',
-			 description: 'The name of the CloudFormation stack to create',
-			 regex: '[-a-zA-Z0-9]+')
-
-
-  }
-
-  options {
-	timestamps()
-  }
-
-
   
   stages{
     stage('Validate Docker') {
       steps {
         sh 'docker info'
-      }
-    }
-    stage('Test Directory'){
-      steps {
-        sh 'ls -la'
       }
     }
 
