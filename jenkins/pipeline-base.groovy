2caf9845f59acb1bce54899ce73876f796869ab0
uonyekwuluje <uionyekwuluje@gmail.com>
2022-10-17 15:28:24 +0000

    update pipeline

--- jenkins/pipeline-base.groovy
+++ jenkins/pipeline-base.groovy
@@ -1,6 +1,17 @@
+def server_classes = [
+    "base-vm",
+    "bastion-vm",
+    "postgres-vm"
+]
+
 pipeline {
   agent any
-  
+
+  parameters {
+        choice name: 'ServerClasses', choices: server_classes, description: ''
+        choice name: 'SubClas', choices: ['Nedi', 'Semmatec', 'Norton'], description: ''
+  }
+
   stages{
     stage('Validate Docker') {
       steps {
@@ -11,7 +22,7 @@ pipeline {
     stage('Test Parameters') {
 	    steps {
 		  script {
-			 def message = "Stack Name => ${params.StackName}\n"
+			 def message = "Stack Name => ${params.ServerClasses}\n"
 			 echo message
 		}
 	  }
